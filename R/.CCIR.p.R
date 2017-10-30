67dec0d6ecd398e28720f0dd246e6f7f3249eb79
amcook <amcook127@gmail.com>
2017-10-30 08:44:07 -0300

    fixing gitignore

--- R/.CCIR.p.R
+++
@@ -1,8 +0,0 @@
-CCIR.p <-function(SublegalLegal,A,B) {
-
-		CumLegal <- cumsum(SublegalLegal[,2])
-		CumLegal <- c(0,CumLegal[1:length(CumLegal)-1])
-		p.hat <- 1/(1 + 1/ (A + B * CumLegal/CumLegal[length(CumLegal)]))
-		return(p.hat)
-	}
-
