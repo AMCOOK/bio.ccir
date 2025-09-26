b7f975e370d30b3d65946848bdf9c90ed964eb36
Zisserson <Ben.Zisserson@dfo-mpo.gc.ca>
2025-09-26 15:21:47 -0300

    removed the time from fsrs data due to TZ conflict

--- R/ccir_stan_summarize.r
+++ R/ccir_stan_summarize.r
@@ -1,6 +1,6 @@
 
 #' @export
-ccir_stan_summarize <- function(x=fit,dat=dat,save=T){
+ccir_stan_summarize <- function(x=fit,dat=dat,save=T, fdir = file.path(project.datadirectory('bio.lobster'),'outputs','ccir','summary')){
 	if(length(x)>1){ fname=x$file; dat = x$dat; x=x$fit}
 	#x is the stan object from ccir_stan
 	#dat is the data list input to stan
@@ -10,7 +10,7 @@ ccir_stan_summarize <- function(x=fit,dat=dat,save=T){
 					 b = apply(a,2,quantile,probs=c(0.025,0.25,0.5,0.75,0.975))
 					 
 			out = (list(LFA= dat$LFA, Yr = dat$Yr, Temp = dat$Temp, Grid = paste(dat$Grid, collapse='.'), Seas = paste(dat$Seas, collapse='.'), Sex = dat$Sex, nSamples = dat$N, dates = dat$dates, N.lobsters.Ref = dat$Nrec, N.lobsters.Exp =dat$Nexp , acceptance.rate = mean(mean_accept_stat_by_chain),ERp = b, ERf = b[,ncol(b)]))
-	    fdir = file.path(project.datadirectory('bio.lobster'),'outputs','ccir','summary')
+	    #fdir = file.path(project.datadirectory('bio.lobster'),'outputs','ccir','summary')
 	  	if(!dir.exists(fdir)) dir.create(fdir,recursive=T,showWarnings=F)
  		fname = paste(fname,'summary','rdata',sep='.')
   		if(save) save(out,file=file.path(fdir,fname))
